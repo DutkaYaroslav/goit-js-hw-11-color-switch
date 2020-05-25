490d3c2a8f6f288be4decf65e03d3b30dcd48e12
unknown <yaroslavdutka@gmail.com>
2020-05-25 13:49:57 +0300

    yup

---
+++ switch.js
@@ -0,0 +1,39 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+
+const refs = {
+    start: document.querySelector('button[data-action="start"]'),
+    stop: document.querySelector('button[data-action="stop"]'),
+    bodyColor: document.querySelector('body'),
+};
+
+let interval;
+
+const startF = function () {
+    interval = setInterval(() => {
+        const randomIntegerFromInterval = (min, max) => {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        };
+
+        refs.bodyColor.style.backgroundColor = `${
+            colors[randomIntegerFromInterval(0, 5)]
+        }`;
+    }, 1000);
+
+    refs.start.setAttribute('disabled', 'true');
+};
+
+refs.start.addEventListener('click', startF);
+
+const stopF = function () {
+    clearInterval(interval);
+    refs.start.removeAttribute('disabled');
+};
+
+refs.stop.addEventListener('click', stopF);
